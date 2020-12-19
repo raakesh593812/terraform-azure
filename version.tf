c2f8c7fcfa5256ab65f754bcae2ad2c4763ec275
Rakesh Yadav <50994371+raakesh593812@users.noreply.github.com>
2020-12-19 12:52:57 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,4 +1,10 @@
 provider "azurerm" {
 version = "2.29.0"
 features {}
+  
+  subscription_id = "cb10d8f8-978c-4bfc-8c34-d26443752202"
+  client_id       = "c2134c22-86ff-41f1-9cc9-c08fd1c73a4e"
+  client_secret   = "eZf6~g-FJgGawu-P7EODEt.z3Z1kcKNW_c"
+  tenant_id       = "777c1949-a60f-47dc-a0d1-457fa0e2b4a6"
+
 }
