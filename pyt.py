253b306fb2da9d41f465a7844ebd96febc19662b
Rakesh Yadav <50994371+raakesh593812@users.noreply.github.com>
2021-09-13 09:36:13 +0530

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+import sys
+
+txt = sys.argv[1]
+
+x=txt.split("/")
+print x[8]
---
+++ workinglist.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+printf '%s\n' vm_name disk_name disk_type | paste -sd ',' > file.csv
+read_vm(){
+ #az disk show --ids "" --query '[].[sku]
+ for a in ${1}
+ do
+#echo "${a}"
+#disk_name = $(az disk show --ids "${a}" --query 'name')
+#echo $(az disk show --ids "${a}" --query 'sku.name')
+#echo $(az disk show --ids "${a}" --query 'managedBy')
+#disk name
+disk_name=$(python2 /home/ramakrishna/pyt.py ${a})
+#vm_name
+vm_name=$(python2 /home/ramakrishna/pyt.py $(az disk show --ids "${a}" --query 'managedBy'))
+#disk type
+disk_type=$(az disk show --ids "${a}" --query 'sku.name')
+printf '%s\n' ${vm_name} ${disk_name} ${disk_type} | paste -sd ',' >> file.csv
+ done
+ #return 1
+}
+
+for a in $(az account list -o tsv --query '[].[id]')
+do
+   az account set --subscription ${a}
+   for line in $(az vm list --query "[].[id]" -o tsv)
+    do
+       #rasx=$(az vm show --ids ${line} --query "storageProfile.dataDisks[].managedDisk.id")
+       echo "====================="
+       read_vm "$(az vm show --ids ${line} --query "storageProfile.dataDisks[].managedDisk.id" -o tsv)"
+    done
+
+done
